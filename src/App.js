f3aee475c4584b5d92e052bfe3eea4709ac3bfea
Peter Czuczor <czuczor@gmail.com>
2022-12-17 23:38:14 +0100

    Second version, working much better.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import logo from './logo.svg';
-
-import './App.css';
 import React, { useState } from 'react';
 import {
   Container,
@@ -12,83 +9,65 @@ import {
   FormControl,
 } from 'react-bootstrap';
 
-function App() {
+const App = () => {
+  const [birthDate, setBirthDate] = useState('');
+  const [weeksInLife, setWeeksInLife] = useState(0);
 
-  const LifeInWeeks = () => {
-    const [birthDate, setBirthDate] = useState('');
-    const [week, setWeek] = useState(1);
-    const [weeksInLife, setWeeksInLife] = useState(52); // number of weeks in a year
-  
-    const handleBirthDateChange = (event) => {
-      setBirthDate(event.target.value);
-    };
-  
-    const handleWeekChange = (event) => {
-      setWeek(event.target.value);
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const ageInWeeks = calculateAgeInWeeks(birthDate, week);
-      setWeeksInLife(ageInWeeks);
-    };
-  
-    const calculateAgeInWeeks = (birthDate, week) => {
-      const today = new Date();
-      const birthYear = birthDate.split('-')[0];
-      const currentYear = today.getFullYear();
-      const ageInYears = currentYear - birthYear;
-      return ageInYears * 52 + week;
-    };
-  
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <h1>Life in Weeks</h1>
-            <p>Enter your birth date and current week to see how many weeks you have lived:</p>
-            <Form onSubmit={handleSubmit}>
-              <FormControl
-                type="date"
-                value={birthDate}
-                onChange={handleBirthDateChange}
-                placeholder="Enter your birth date"
-              />
-              <FormControl
-                type="number"
-                value={week}
-                onChange={handleWeekChange}
-                placeholder="Enter your current week"
-              />
-              <Button variant="primary" type="submit">
-                Calculate
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-        {weeksInLife > 0 && (
-          <Row>
-            <Col>
-              <h2>You have lived for {weeksInLife} weeks</h2>
-            </Col>
-          </Row>
-        )}
-        <Row>
-          {[...Array(weeksInLife).keys()].map((weekNumber) => (
-            <Col key={weekNumber} xs={1}>
-              <Card>
-                <Card.Body>Week {weekNumber + 1}</Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    );
+  const handleBirthDateChange = (event) => {
+    setBirthDate(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const ageInWeeks = calculateAgeInWeeks(birthDate);
+    setWeeksInLife(ageInWeeks);
+  };
+
+  const calculateAgeInWeeks = (birthDate) => {
+    const today = new Date();
+    const birthDateTimestamp = new Date(birthDate).getTime();
+    const ageInMilliseconds = today - birthDateTimestamp;
+    const ageInWeeks = Math.floor(ageInMilliseconds / (7 * 24 * 60 * 60 * 1000));
+    return ageInWeeks;
   };
 
   return (
-    <LifeInWeeks />
-  )
-}
+    <Container>
+      <Row>
+        <Col>
+          <h1>Life in Weeks</h1>
+          <p>Enter your birth date to see how many weeks you have lived:</p>
+          <Form onSubmit={handleSubmit}>
+            <FormControl
+              type="date"
+              value={birthDate}
+              onChange={handleBirthDateChange}
+              placeholder="Enter your birth date"
+            />
+            <Button variant="primary" type="submit">
+              Calculate
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      {weeksInLife > 0 && (
+        <Row>
+          <Col>
+            <h2>You have lived for {weeksInLife} weeks</h2>
+          </Col>
+        </Row>
+      )}
+      <Row>
+        {[...Array(weeksInLife).keys()].map((weekNumber) => (
+          <Col key={weekNumber} xs={1}>
+            <Card>
+              <Card.Body>Week {weekNumber + 1}</Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 
 export default App;
